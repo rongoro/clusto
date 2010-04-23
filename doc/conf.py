7abb50a1907beef2566a1c8b8ea2012b96c085e9
Jeremy Grosser <synack@digg.com>
2010-04-23 13:20:08 -0700

    Fix copyright in license file, version bump

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ copyright = '2010, Digg, Inc.'
 # The short X.Y version.
 version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.22'
+release = '0.5.23'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 srcdir = os.path.join(os.path.dirname(sys.argv[0]), 'src')
 
 setup(name = "clusto",
-      version = "0.5.22",
+      version = "0.5.23",
       packages = find_packages('src'),
       author = "Ron Gorodetzky",
       author_email = "ron@digg.com",
