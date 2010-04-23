0a1654a71f4a5421c8ca197c01c2e19f3bfbdf89
Jeremy Grosser <synack@digg.com>
2010-04-23 12:50:18 -0700

    Update docs version

--- doc/conf.py
+++ doc/conf.py
@@ -43,9 +43,9 @@ copyright = '2010, Digg, Inc.'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.5.8'
+version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.8'
+release = '0.5.22'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
