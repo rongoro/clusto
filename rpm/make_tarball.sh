ccce108354ca4264c69d5d28716eeccc269db1a7
Jorge A Gallegos <kad@blegh.net>
2010-05-05 16:04:44 +0800

    Use MAKE instead of lasttag

--- rpm/make_tarball.sh
+++ rpm/make_tarball.sh
@@ -5,7 +5,7 @@ version=$( echo $lasttag | awk -F- '{ print $2 }' )
 prefix="clusto-${version}"
 
 echo "Appending prefix $prefix..."
-git archive --format=tar --prefix=$prefix/ $lasttag > ./$(git rev-parse --show-cdup)/clusto-$version.tar
+git archive --format=tar --prefix=$prefix/ HEAD > ./$(git rev-parse --show-cdup)/clusto-$version.tar
 tar -C $(dirname $0 ) --transform="s@clusto.spec@clusto-${version}/clusto.spec@" -rvf ./$(git rev-parse --show-cdup)/clusto-$version.tar clusto.spec
 gzip ./$(git rev-parse --show-cdup)/clusto-$version.tar
 echo "Wrote clusto-$version.tar.gz"
