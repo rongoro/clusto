036d03f3f713b81252c4c5bff0ee690ce38a35c9
Jorge A Gallegos <kad@blegh.net>
2010-05-07 13:07:47 +0800

    Check .spec changelog for changes

--- rpm/make_tarball.sh
+++ rpm/make_tarball.sh
@@ -5,7 +5,5 @@ version=$( echo $lasttag | awk -F- '{ print $2 }' )
 prefix="clusto-${version}"
 
 echo "Appending prefix $prefix..."
-git archive --format=tar --prefix=$prefix/ HEAD > ./$(git rev-parse --show-cdup)/clusto-$version.tar
-tar -C $(dirname $0 ) --transform="s@clusto.spec@clusto-${version}/clusto.spec@" -rvf ./$(git rev-parse --show-cdup)/clusto-$version.tar clusto.spec
-gzip ./$(git rev-parse --show-cdup)/clusto-$version.tar
+git archive --format=tar --prefix=$prefix/ HEAD | gzip > ./$(git rev-parse --show-cdup)/rpm/clusto-$version.tar.gz
 echo "Wrote clusto-$version.tar.gz"
