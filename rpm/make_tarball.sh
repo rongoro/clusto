182f5228b91a959e2ef622a2d79700971a73f62a
Jorge A Gallegos <kad@blegh.net>
2010-05-07 13:13:44 +0800

    Fixing tarball paths.

--- rpm/make_tarball.sh
+++ rpm/make_tarball.sh
@@ -4,6 +4,8 @@ lasttag="$( git describe --tags --abbrev=0 )"
 version=$( echo $lasttag | awk -F- '{ print $2 }' )
 prefix="clusto-${version}"
 
+cd ./$(git rev-parse --show-cdup)
 echo "Appending prefix $prefix..."
 git archive --format=tar --prefix=$prefix/ HEAD | gzip > ./$(git rev-parse --show-cdup)/rpm/clusto-$version.tar.gz
 echo "Wrote clusto-$version.tar.gz"
+cd $OLDPWD
